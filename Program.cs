115000a1398dea663cff3f09096834c2882ac94a
Dydha MPANDOU <dmpandou@afludia.com>
2026-01-06 16:09:05 +0100

    modif dark mode

--- Program.cs
+++ Program.cs
@@ -1,18 +1,16 @@
 using Blazored.LocalStorage;
-using BlazorThemeSwitcher.Contracts;
-using BlazorThemeSwitcher.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using MyPortfolio;
+using MyPortfolio.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped<IThemePreferenceStorage, LocalStorageThemePreferenceStorage>();
-builder.Services.AddScoped<ThemeService>();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<ThemeService>();
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ Services/ThemeService.cs
@@ -0,0 +1,64 @@
+using Blazored.LocalStorage;
+using MudBlazor;
+
+namespace MyPortfolio.Services;
+
+public class ThemeService
+{
+    private readonly ILocalStorageService _localStorage;
+    private MudTheme _currentTheme;
+    private bool _isDarkMode;
+
+    public event Action<bool>? ThemeChanged;
+
+    public MudTheme CurrentTheme => _currentTheme;
+    public bool IsDarkMode => _isDarkMode;
+
+    public ThemeService(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+        _currentTheme = new MudTheme()
+        {
+            PaletteLight = new PaletteLight()
+            {
+                Primary = Colors.Blue.Default,
+                Secondary = Colors.Green.Default,
+                AppbarBackground = Colors.Blue.Default,
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = Colors.Blue.Lighten1,
+                Secondary = Colors.Green.Lighten1,
+                AppbarBackground = Colors.Blue.Darken1,
+            }
+        };
+    }
+
+    public async Task InitializeAsync()
+    {
+        try
+        {
+            var savedTheme = await _localStorage.GetItemAsStringAsync("theme-preference");
+            _isDarkMode = savedTheme == "dark";
+        }
+        catch
+        {
+            _isDarkMode = false; // Par défaut, mode clair
+        }
+        await NotifyThemeChanged();
+    }
+
+    public async Task ToggleThemeAsync()
+    {
+        _isDarkMode = !_isDarkMode;
+        await _localStorage.SetItemAsStringAsync("theme-preference", _isDarkMode ? "dark" : "light");
+        await NotifyThemeChanged();
+    }
+
+    private async Task NotifyThemeChanged()
+    {
+        ThemeChanged?.Invoke(_isDarkMode);
+        await Task.CompletedTask;
+    }
+}
+
